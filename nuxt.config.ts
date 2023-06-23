6ee5ddb9ca42907b7c4f613b1f627617d7260b04
Sanjay Soundarajan <sanjay.16@live.com>
2023-06-23 13:45:19 -0700

    chore: testing umami

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,17 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
+  extends: ["nuxt-umami"],
+
+  appConfig: {
+    umami: {
+      host: "https://umami.fairdataihub.org/mushroom",
+      id: "4695e60b-3e6b-4c8c-9c09-b93140265367",
+      customEndpoint: "/api/harvest",
+      version: "2",
+    },
+  },
+
   modules: ["@bg-dev/nuxt-naiveui", "@nuxtjs/tailwindcss"],
 
   naiveui: {
